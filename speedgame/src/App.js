85a66044c844b4a71f353096ce84d92b7374f63d
hinnurainen <heidi.maattanen@gmail.com>
2021-04-08 14:58:20 +0300

    speedgame first steps

---
+++ speedgame/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Circle from "./Components/Circle";
+
+import "./App.css";
+
+class App extends Component {
+  state = {
+    circles: [
+      { id: 1, color: "green" },
+      { id: 2, color: "yellow" },
+      { id: 3, color: "red" },
+      { id: 4, color: "orange" }
+    ],
+  };
+
+  render() {
+    const circlesList = this.state.circles.map((c) => {
+      return <Circle id={c.id} key={c.id} color={c.color} />;
+    });
+    return (
+      <div>
+        <h1>Speedgame</h1>
+        <p>Your score is:</p>
+        <div className="circles">
+          {circlesList}</div>
+        <button>Start</button>
+        <button>Stop</button>
+      </div>
+    );
+  }
+}
+
+export default App;
