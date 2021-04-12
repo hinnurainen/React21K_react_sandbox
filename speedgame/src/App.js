64d3055ee20fc9e912ebcbb38b3c33ae98d1f1c5
hinnurainen <heidi.maattanen@gmail.com>
2021-04-12 11:51:13 +0300

    speedgame mid additions

--- speedgame/src/App.js
+++ speedgame/src/App.js
@@ -3,8 +3,14 @@ import Circle from "./Components/Circle";
 
 import "./App.css";
 
+const getRndInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 class App extends Component {
   state = {
+    score: 0,
+    current: 0,
     circles: [
       { id: 1, color: "green" },
       { id: 2, color: "yellow" },
@@ -13,18 +19,51 @@ class App extends Component {
     ],
   };
 
+  timer = undefined;
+  pace = 1500;
+
+  clickHandler = (id) => {
+    console.log("wow, you clicked a circle " + id);
+    this.setState({
+      score: this.state.score + 1,
+    })
+  }
+
+  nextCircle = () => {
+    let nextActive = undefined;
+
+    do {
+      nextActive = getRndInteger(1, 4);
+    } while (nextActive === this.state.current);
+
+    this.setState({ current: nextActive, });
+
+    this.pace *= 0.95;
+    this.timer = setTimeout(this.nextCircle, this.pace);
+
+    console.log("active circle is ", this.state.current);
+  };
+
+  startHandler = () => {
+    this.nextCircle();
+  }
+
+  endHandler = () => {
+    clearTimeout(this.timer);
+  }
+
   render() {
     const circlesList = this.state.circles.map((c) => {
-      return <Circle id={c.id} key={c.id} color={c.color} />;
+      return <Circle id={c.id} key={c.id} color={c.color} click={() => this.clickHandler(c.id)} />;
     });
     return (
       <div>
         <h1>Speedgame</h1>
-        <p>Your score is:</p>
+        <p>Your score is: {this.state.score}</p>
         <div className="circles">
           {circlesList}</div>
-        <button>Start</button>
-        <button>Stop</button>
+        <button onClick={this.startHandler}>Start</button>
+        <button onClick={this.endHandler}>Stop</button>
       </div>
     );
   }
