e96749ff1eaec5b5db7609746cbe245757bf5585
hinnurainen <heidi.maattanen@gmail.com>
2021-08-10 11:31:22 +0300

    list functionality ok

--- todo/src/Components/Main.js
+++ todo/src/Components/Main.js
@@ -1,21 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
 import { InputGroup, FormControl, Button } from 'react-bootstrap';
 
-const Main = () => {
+function Main() {
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = todo => {
+        if (!todo.text || /^\s*$/.test(todo.text)) {
+            return;
+        }
+
+        const newTodos = [todo, ...todos];
+
+        setTodos(newTodos);
+        console.log(...todos);
+    };
+
+    const updateTodo = (todoId, newValue) => {
+        if (!newValue.text || /^\s*$/.test(newValue.text)) {
+            return;
+        }
+
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+    };
+
+    const removeTodo = id => {
+        const removedArr = [...todos].filter(todo => todo.id !== id);
+
+        setTodos(removedArr);
+    };
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+    };
+
     return (
-        <div>
-            <InputGroup className="mb-3">
-                <FormControl
-                    placeholder="Add a task..."
-                    aria-label="Add a task..."
-                    aria-describedby="basic-addon2"
-                />
-                <Button variant="outline-secondary" id="button-addon2">
-                    Submit
-                </Button>
-            </InputGroup>
-        </div>
+        <>
+            <h1>What's the Plan for Today?</h1>
+            <TodoForm onSubmit={addTodo} />
+            <Todo
+                todos={todos}
+                completeTodo={completeTodo}
+                removeTodo={removeTodo}
+                updateTodo={updateTodo}
+            />
+        </>
     );
-};
+}
 
 export default Main;
